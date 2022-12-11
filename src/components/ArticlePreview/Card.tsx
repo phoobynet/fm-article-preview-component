37105c77a1ae72ff9cf70503c87e620b4600371d
Glenn <phoobynet@users.noreply.github.com>
2022-12-11 15:17:12 +0000

    wip

---
+++ src/components/ArticlePreview/Card.tsx
@@ -0,0 +1,36 @@
+import styles from './Card.module.scss'
+import drawers from '@/assets/images/drawers.jpg'
+import Author from '@/components/ArticlePreview/Author'
+import Share from '@/components/ArticlePreview/Share'
+
+export default function Card() {
+  return (
+    <div className={styles.card}>
+      <header className={styles.cardHeader}>
+        <img
+          src={drawers}
+          alt=""
+        />
+      </header>
+      <main className={styles.cardMain}>
+        <h1>
+          Shift the overall look and feel by adding these wonderful touches to
+          furniture in your home
+        </h1>
+        <p>
+          Ever been in a room and felt like something was missing? Perhaps it
+          felt slightly bare and uninviting. I’ve got some simple tips to help
+          you make any room feel complete.
+        </p>
+      </main>
+      <footer className={styles.cardFooter}>
+        <div className={styles.cardFooterNormal}>
+          <Author />
+        </div>
+        <div className={styles.cardFooterAlt}>
+          <Share />
+        </div>
+      </footer>
+    </div>
+  )
+}
--- src/routes/ArticlePreview.tsx
+++ src/routes/ArticlePreview.tsx
@@ -1,42 +1,10 @@
 import styles from './ArticlePreview.module.scss'
-import drawers from '@/assets/images/drawers.jpg'
-import Author from '@/components/ArticlePreview/Author'
-import Share from '@/components/ArticlePreview/Share'
-import { useRef, useState } from 'react'
+import Card from '@/components/ArticlePreview/Card'
 
 export default function ArticlePreview() {
-  const footer = useRef<HTMLDivElement>(null)
-  const [showShare, setShowShare] = useState(false)
-
   return (
     <div className={styles.articlePreview}>
-      <div className={styles.container}>
-        <header>
-          <img
-            src={drawers}
-            alt=""
-          />
-        </header>
-        <main>
-          <h1>
-            Shift the overall look and feel by adding these wonderful touches to
-            furniture in your home
-          </h1>
-          <p>
-            Ever been in a room and felt like something was missing? Perhaps it
-            felt slightly bare and uninviting. I’ve got some simple tips to help
-            you make any room feel complete.
-          </p>
-        </main>
-        <footer>
-          <div className={styles.normal}>
-            <Author />
-          </div>
-          <div className={styles.alt}>
-            <Share />
-          </div>
-        </footer>
-      </div>
+      <Card />
     </div>
   )
 }
