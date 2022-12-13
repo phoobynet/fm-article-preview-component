6e20318bc240bfc1515a4dc2d54a937109801471
Glenn <phoobynet@users.noreply.github.com>
2022-12-13 21:42:27 +0000

    Lost!

--- src/components/ArticleFooter.tsx
+++ src/components/ArticleFooter.tsx
@@ -1,18 +1,10 @@
 import styles from './ArticleFooter.module.scss'
 import ArticleAuthor from '@/components/ArticleAuthor'
 import ArticleShare from '@/components/ArticleShare'
-import { useState } from 'react'
-import { createPortal } from 'react-dom'
 
 export default function ArticleFooter() {
-  const [showTooltip, setShowTooltip] = useState(false)
-
   return (
-    <footer
-      className={styles.articleFooter}
-      onMouseEnter={() => setShowTooltip(true)}
-      onMouseLeave={() => setShowTooltip(false)}
-    >
+    <footer className={styles.articleFooter}>
       <div className={styles.normal}>
         <ArticleAuthor />
       </div>
--- src/routes/Demo.tsx
+++ src/routes/Demo.tsx
@@ -1,20 +1,69 @@
-import styles from './Demo.module.scss'
-import share from '@/assets/images/icon-share.svg'
+import { PropsWithChildren, useEffect, useRef, useState } from 'react'
+import { createPortal } from 'react-dom'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: 70%;
+  margin: 3rem auto 0;
+`
+
+const Popover = styled.div`
+  font-size: 1.5rem;
+  position: absolute;
+  height: 50px;
+  width: 100px;
+  top: 0;
+  background-color: #000;
+  opacity: 0.5;
+  color: #f9fff9;
+`
+
+const RenderPortal = ({ children }: PropsWithChildren) => {
+  return createPortal(children, document.getElementById('portal')!)
+}
 
 export default function Demo() {
+  const [show, setShow] = useState(false)
+  const [x, setX] = useState(0)
+  const [y, setY] = useState(0)
+  const [width, setWidth] = useState(0)
+  const pop = useRef<HTMLDivElement>(null)
+
+  const buttonRef = useRef<HTMLButtonElement>(null)
+  const buttonRect = useRef<DOMRect>()
+
+  const handleButtonRef = (el: HTMLButtonElement) => {
+    if (!el) return
+
+    const domRect = el.getBoundingClientRect()
+    setX(domRect.x)
+    setY(domRect.y)
+    setWidth(domRect.width)
+  }
+
+  useEffect(() => {
+    buttonRect.current = buttonRef.current?.getBoundingClientRect()
+  }, [])
+
+  const onClick = () => {
+    setShow((state) => !state)
+  }
+
   return (
-    <div className={styles.demo}>
-      <main>
-        <div data-popover-target>
-          <img
-            src={share}
-            alt=""
-          />
-          <div data-popover>
-            <p>I&apos;m a popover</p>
-          </div>
-        </div>
-      </main>
-    </div>
+    <Container>
+      <RenderPortal>{show && <Popover ref={pop}>Hello</Popover>}</RenderPortal>
+      <button
+        onClick={onClick}
+        ref={handleButtonRef}
+      >
+        Click Me
+      </button>
+
+      {x && (
+        <p>
+          x: {x}, y: {y}, width: {width}
+        </p>
+      )}
+    </Container>
   )
 }
