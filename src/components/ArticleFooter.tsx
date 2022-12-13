f9c60736af1845c228beb6af1461ddfc66e7a67f
Glenn <phoobynet@users.noreply.github.com>
2022-12-13 14:39:19 +0000

    Responsive work

--- src/components/ArticleFooter.tsx
+++ src/components/ArticleFooter.tsx
@@ -1,10 +1,18 @@
 import styles from './ArticleFooter.module.scss'
 import ArticleAuthor from '@/components/ArticleAuthor'
 import ArticleShare from '@/components/ArticleShare'
+import { useState } from 'react'
+import { createPortal } from 'react-dom'
 
 export default function ArticleFooter() {
+  const [showTooltip, setShowTooltip] = useState(false)
+
   return (
-    <footer className={styles.articleFooter}>
+    <footer
+      className={styles.articleFooter}
+      onMouseEnter={() => setShowTooltip(true)}
+      onMouseLeave={() => setShowTooltip(false)}
+    >
       <div className={styles.normal}>
         <ArticleAuthor />
       </div>
